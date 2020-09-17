e07830e187f3e2b3119f1fa81beb7c4fba649054
kevocde <kevocde@gmail.com>
2020-09-17 08:03:08 -0500

    Modificada ubicación de plantilla y creada implementación parcial de página en blanco

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="horizontalMenucontainer" id="app">
+    <div id="global-loader">
+			<img src="../public/valex-theme/html/assets/img/loader.svg" class="loader-img" alt="Loader">
+		</div>
+    <div class="page">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view/>
+      <footer-component></footer-component>
     </div>
-    <router-view/>
+    <a href="#top" id="back-to-top"><i class="las la-angle-double-up"></i></a>
   </div>
 </template>
+<script>
+import FooterComponent from './components/FooterComponent.vue'
+
+export default {
+  name: 'app',
+  components: {FooterComponent}
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="main-footer ht-40">
+		<div class="container-fluid pd-t-0-f ht-100p">
+			<span>Copyright © 2020 <a href="#">Valex</a>. Designed by <a href="https://www.spruko.com/">Spruko</a> All rights reserved.</span>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+    name: 'footer-component'
+}
+</script>
