2e2e9b5883dc295d1c37fd1498f539cb842b7796
kevocde <kevocde@gmail.com>
2020-09-25 09:37:57 -0500

    Creada estructura de administración de archivos

---
+++ src/components/entities/EntityItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="entity-item-component d-flex justity-content-center">
+        <div class="card custom-card card-body p-1">
+            <p class="mb-0 eic-icon text-secondary"><i class="fas fa-folder"></i></p>
+            <div class="card-header tx-medium p-1 text-white">{{entity.alias}}</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'entity-item',
+    props: {
+        entity: {
+            type: Object,
+            default: () => []
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .entity-item-component {
+        .card {
+            cursor: pointer;
+
+            .eic-icon i {
+                transition: 200ms all ease;
+                font-size: 4rem;
+            }
+
+            &:hover {
+                .eic-icon i {
+                    color: #fff;
+                }
+            }
+        }
+    }
+</style>
---
+++ src/views/ListEntities.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="list-entities-component">
+        <div class="row">
+            <div class="col-12 col-md-4 col-lg-2">
+                <nav class="nav nav-pills flex-column">
+                    <a class="nav-link active" href="#">Mi unidad</a>
+                    <a class="nav-link" href="#">Papelera</a>
+                </nav>
+                <hr>
+            </div>
+            <div class="col-12 col-md-8 col-md-10">
+                <div class="row">
+                    <div class="col-12">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb breadcrumb-style2">
+                                <li class="breadcrumb-item"><a href="#">Mi unidad</a></li>
+                                <li class="breadcrumb-item active"><a href="#">/</a></li>
+                            </ol>
+                        </nav>
+                    </div>
+                    <entity-item v-for="entity of entities" :key="entity.driver_id" :entity="entity" class="col-6 col-sm-4 col-md-3 col-lg-2"></entity-item>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import EntityItem from '../components/entities/EntityItem'
+
+export default {
+    name: 'list-entities',
+    data: () => {
+        return {
+            entities: [
+                {driver_id: 1, parent_entity_id: null, type: 'D', name: 'x1wer', alias: 'Documentos', path: '/'},
+                {driver_id: 2, parent_entity_id: null, type: 'D', name: 'x2wer', alias: 'Imagenes', path: '/'},
+                {driver_id: 3, parent_entity_id: null, type: 'D', name: 'x3wer', alias: 'Videos', path: '/'},
+            ]
+        }
+    },
+    components: {EntityItem}
+}
+</script>
+<style lang="scss" scoped>
+    .list-entities-component {
+        .nav .nav-link {
+            text-align: start;
+        }
+    }
+</style>
